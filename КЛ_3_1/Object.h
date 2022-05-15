1a9d4fac8b93072012836b8a46a959faf240e04c
Waalfegor <100232625+Waalfegor@users.noreply.github.com>
2022-05-15 03:13:47 +0300

    Update Object.h

--- КЛ_3_1/Object.h
+++ КЛ_3_1/Object.h
@@ -1,15 +1,11 @@
 #ifndef OBJECT_H
 #define OBJECT_H
-
 #include <iostream>
 #include <vector>
 #include <string>
-
 using namespace std;
-
 class Object
 {
-	
 private:
 
 	string name;
@@ -20,6 +16,7 @@ protected:
 
 	std::vector<Object*> children;
 
+
 public:
 	
 	Object(Object*, string = "root");
@@ -28,12 +25,17 @@ public:
 	Object* get_parent();
 	void set_name(string);
 	string get_name();
+
+	// вывод иерархии
 	void print_child();
 	void print();
 	
-
+	// вывод статуса
+	void is_ready();
 	void set_status(int status);
+	void print_status_child();
 	void print_status_tree();
+
 	void add_child(Object* h);
 	Object* get_by_name(std::string name);
 
